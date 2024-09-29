ecd5de310ede6b44ce3ef00713d05c1c998ce9ca
yponion <yp071704@gmail.com>
2024-09-29 22:19:03 +0900

    feat: 반응형 디자인
     - width에 따른 display 변경
     - screen mode에 따른 hover 색상 변경

--- src/components/Information.tsx
+++ src/components/Information.tsx
@@ -9,12 +9,16 @@ interface InformationProps {
 const Information: React.FC<InformationProps> = ({ h2, contents }) => {
   return (
     <div className="information-container">
-      <h2>{h2}</h2>
+      <h2 className="info-h2">{h2}</h2>
       {contents.map((content, i) => (
         <>
           <div className="information-inner">
-            <p key={content + "0"}>{content[0]}</p>
-            <p key={content + "1"}>{content[1]}</p>
+            <p className="info-title" key={content + "0"}>
+              {content[0]}
+            </p>
+            <p className="info-content" key={content + "1"}>
+              {content[1]}
+            </p>
           </div>
           {i < contents.length - 1 && <hr className="user-hr"></hr>}
         </>
@@ -22,5 +26,8 @@ const Information: React.FC<InformationProps> = ({ h2, contents }) => {
     </div>
   );
 };
+
+// todo 비밀번호 변경 누르면 아래에 변경 펼쳐지게
+// todo 탈퇴 누르면 아래에 정말 탈퇴하시겠습니까 버튼 슈루룩
 
 export default Information;
