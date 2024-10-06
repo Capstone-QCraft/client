735c9589e4975e53939a3ec63bb6dd7697152aed
yponion <yp071704@gmail.com>
2024-10-06 14:13:54 +0900

    feat: 음성인식 정지

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,30 +1,55 @@
 import React from "react";
 import "./Chat.css";
 import mic from "../assets/images/microphone.png";
+import stop from "../assets/images/stop.png";
+import useSpeechRecognition from "../hooks/useSpeechRecognition";
 
 interface ChatProps {
   question: string;
   answer: string;
+  onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  isHistory?: boolean;
 }
 
-const Chat: React.FC<ChatProps> = ({ question, answer }) => {
+const Chat: React.FC<ChatProps> = ({
+  question,
+  answer,
+  onChange,
+  isHistory,
+}) => {
+  const { text, isListening, startListening, stopListening } =
+    useSpeechRecognition(); // todo 음성 입력 시 텍스트 입력 차단 -> [...answer, text]
+
   return (
     <div className="chat-container">
       <div className="chat-inner chat-q">
         <p className="chat-content">{question}</p>
       </div>
-      <div className="chat-inner chat-a">
-        <p className="chat-content">{answer}</p>
-      </div>
-      <div className="chat-inner chat-a">
-        <textarea
-          className="chat-textarea"
-          placeholder="이곳에 채팅 또는 음성으로 답변 입력"
-        />
-        <button className="chat-mic-button">
-          <img src={mic} alt="microphone" className="chat-mic-img" />
-        </button>
-      </div>
+      {isHistory && (
+        <div className="chat-inner chat-a">
+          <p className="chat-content">{answer}</p>
+        </div>
+      )}
+      {!isHistory && (
+        <div className="chat-inner chat-a">
+          <textarea
+            className="chat-textarea"
+            placeholder="이곳에 채팅 또는 음성으로 답변 입력"
+            value={answer}
+            onChange={onChange}
+          />
+          <button
+            className="chat-mic-button"
+            onClick={isListening ? stopListening : startListening}
+          >
+            <img
+              src={isListening ? stop : mic}
+              alt="microphone"
+              className="chat-mic-img"
+            />
+          </button>
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/AIPage.tsx
+++ src/pages/AIPage.tsx
@@ -4,6 +4,15 @@ import Chat from "../components/Chat";
 import "./AIPage.css";
 
 const AIPage = () => {
+  const questions = ["질문1", "질문2", "질문3", "질문4", "질문5"];
+  const [answers, setAnswers] = useState<string[]>(["", "", "", "", ""]);
+
+  const handleChange = (index: number, value: string) => {
+    const newValues = [...answers];
+    newValues[index] = value;
+    setAnswers(newValues);
+  };
+
   return (
     <HelmetProvider>
       <Helmet>
@@ -11,11 +20,19 @@ const AIPage = () => {
       </Helmet>
       <div className="ai-container">
         <div className="ai-inner">
-          {/* <Chat question="밥 드셨나요?" answer="그럼요~!" /> */}
-          <Chat
+          {questions.map((v, i) => (
+            <Chat
+              key={i}
+              question={v}
+              answer={answers[i]}
+              onChange={(e) => handleChange(i, e.target.value)}
+            />
+          ))}
+          {/* <Chat
             question="팀 프로젝트를 진행하면서 어려운 점은 무엇이었으며, 어떻게 해결하여 나갔나요?"
             answer="팀프로젝트를 진행하면서 어려웠던 점은 ~ 이고, 그 것을 ~로 해결하였습니다."
-          />
+            isHistory={true}
+          /> */}
         </div>
       </div>
     </HelmetProvider>
