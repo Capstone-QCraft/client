1d3041bf56952615675302706b8251bfa8707ed0
yponion <yp071704@gmail.com>
2024-10-11 00:07:00 +0900

    feat: histories page 구상중..

--- src/pages/HistoriesPage.tsx
+++ src/pages/HistoriesPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HelmetProvider, Helmet } from "react-helmet-async";
+import "./HistoriesPage.css";
 
 const HistoriesPage = () => {
   return (
@@ -7,7 +8,13 @@ const HistoriesPage = () => {
       <Helmet>
         <title>기록</title>
       </Helmet>
-      <div className="">HistoriesPage</div>
+      <div className="histories-container">
+        <div className="histories-inner">
+          <div>2024.10.10, 프론트엔드, 입사지원서-양정운.pdf</div>
+          <div>2024.10.09, 프론트엔드, 입사지원서-양정운.word</div>
+          <div>2024.01.01, 백엔드, 자기소개서.pdf</div>
+        </div>
+      </div>
     </HelmetProvider>
   );
 };
