5f6044e06215c754c9e84f8499a81257305be854
yponion <yp071704@gmail.com>
2024-10-21 14:44:32 +0900

    feat: kakao, google, naver oauth 로그인 디자인, 구현 완료
    naver는 개발자만 사용가능해서 테스트 못해봄

--- src/components/OauthButton.tsx
+++ src/components/OauthButton.tsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./OauthButton.css";
+import { useNavigate } from "react-router-dom";
+
+import google_logo from "../assets/images/google.svg";
+import kakao_logo from "../assets/images/kakao.svg";
+import naver_light_logo from "../assets/images/naver_light.svg";
+import naver_dark_logo from "../assets/images/naver_dark.svg";
 
 interface OauthButtonProps {
   type: "kakao" | "google" | "naver";
 }
 
+const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+
 const OauthButton: React.FC<OauthButtonProps> = ({ type }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    setIsDarkMode(mediaQuery.matches);
+
+    const handleChange = (event: MediaQueryListEvent) => {
+      setIsDarkMode(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleChange);
+    return () => mediaQuery.removeEventListener("change", handleChange);
+  }, []);
+
   const getButtonLabel = () => {
     switch (type) {
       case "kakao":
@@ -19,12 +42,31 @@ const OauthButton: React.FC<OauthButtonProps> = ({ type }) => {
     }
   };
 
+  const getButtonLogo = () => {
+    switch (type) {
+      case "kakao":
+        return kakao_logo;
+      case "google":
+        return google_logo;
+      case "naver":
+        return isDarkMode ? naver_dark_logo : naver_light_logo;
+    }
+  };
+
   return (
-    <div className={`oauth-button-container ${type}`}>
-      <a href={`/${type}-login`} className="oauth-button">
-        {getButtonLabel()}
-      </a>
-    </div>
+    <button
+      className={`oauth-button-container oauth-button-${type}`}
+      onClick={() =>
+        (window.location.href = `${SERVER_URL}/member/oauth2/${type}`)
+      }
+    >
+      <img
+        className="oauth-button-img"
+        src={getButtonLogo()}
+        alt={getButtonLabel()}
+      />
+      {getButtonLabel()}
+    </button>
   );
 };
 
